f099ed2c2ddfe98806806e9c4a70fea8e3a19ea2
Anullos <anullos988@gmail.com>
2021-03-13 23:26:05 +0100

    primreo

---
+++ src/js/casa_anim.js
@@ -0,0 +1,15 @@
+// const imagen = document.getElementById("home__imagen");
+// const pulsador = document.getElementById("pulsador");
+
+// pulsador.addEventListener("click", () => {
+//   //   console.log("pulsando en la casa");
+//   //     imagen.style.width = "100%";
+//   //     imagen.style.height = "100%";
+//   //     imagen.style.top = "0";
+//   //     imagen.style.bottom = "0";
+//   //     imagen.style.left = "0";
+//   //     imagen.style.right = "0";
+//   pulsador.style.backgroundColor = "red";
+//   imagen.style.width = "200%";
+//   imagen.style.height = "200%";
+// });
---
+++ src/js/principal.js
@@ -0,0 +1,54 @@
+const home = document.getElementById("home");
+const container_house = document.getElementById("container_house");
+const transition_house = document.getElementById("transition_house");
+
+function cambioPantalla(menu) {
+  switch (menu) {
+    case "home":
+      if (home.classList.contains("ocultar")) {
+        home.classList.toggle("ocultar");
+        container_house.classList.toggle("ocultar");
+      }
+      break;
+    case "about":
+      if (container_house.classList.contains("ocultar")) {
+        transition_house.classList.add("aumentar");
+        setTimeout(function () {
+          transition_house.classList.add("ocultar");
+        }, 2800);
+        setTimeout(function () {
+          home.classList.toggle("ocultar");
+          container_house.classList.toggle("ocultar");
+          document.getElementById("final").scrollIntoView(true);
+        }, 1200);
+      }
+      break;
+    case "portafolio":
+      if (container_house.classList.contains("ocultar")) {
+        home.classList.toggle("ocultar");
+        container_house.classList.toggle("ocultar");
+      }
+      break;
+    case "contact":
+      if (container_house.classList.contains("ocultar")) {
+        home.classList.toggle("ocultar");
+        container_house.classList.toggle("ocultar");
+      }
+      break;
+
+    default:
+      break;
+  }
+}
+
+function aumentar() {
+  transition_house.classList.add("aumentar");
+  setTimeout(function () {
+    transition_house.classList.add("ocultar");
+  }, 2800);
+  setTimeout(function () {
+    home.classList.toggle("ocultar");
+    container_house.classList.toggle("ocultar");
+    document.getElementById("final").scrollIntoView(true);
+  }, 1200);
+}
